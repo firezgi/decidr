a55493309f608bf09997a315d469baad70b63e96
Habteab <keshi2017@gmail.com>
2021-11-30 11:32:47 -0800

    chore(gh-pages): add gh-pages and scripts

--- src/App.js
+++ src/App.js
@@ -3,24 +3,10 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [listContainer,setListContainer]=useState
-  ([])
+  const [listContainer,setListContainer]=useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello</p>
     </div>
   );
 }
