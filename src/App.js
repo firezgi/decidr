00b7e65f94cb85d43723dd3ca6c277bff62cfc29
Habteab <keshi2017@gmail.com>
2021-11-30 14:28:16 -0800

    chore(select random): add random item function#10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import AddItem from './components/AddItem';
 
 function App() {
   const [listContainer,setListContainer]=useState([]);
+  const [selectedItem,setSelectedItem]=useState("");
+  const selectItem=()=>{
+    const random=Math.floor(Math.random()*listContainer.length);
+    setSelectedItem(listContainer[random]);
+  }
   return (
     <div className="App">
-      <p>Hello</p>
+      <header>Decidr</header>
+
+
+      <AddItem
+      listContainer={listContainer} 
+      setListContainer={setListContainer} />
     </div>
   );
 }
---
+++ src/components/AddItem.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function AddItem({listContainer,setListContainer}) {
+    const[newItem,setNewItem]=useState("");
+    const formSubmit=(event)=>{
+        event.preventDefault();
+        setListContainer([...listContainer,newItem])
+        setNewItem("")
+        
+    }
+    return (
+        <form onSubmit={formSubmit}>
+            <input 
+            id="add-item" 
+            value={newItem} 
+            onChange={e=>setNewItem(e.target.value)}
+            placeholder="Add an item"
+            />
+            <button id="submit-item" onClick={formSubmit} type="submit" >Add Item</button>
+            <div>
+            {listContainer}
+                </div>
+        </form>
+    )
+}
+
+export default AddItem
