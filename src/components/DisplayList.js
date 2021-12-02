42d54d353cb06ba9d11bea58b193a5eadf924e87
Habteab <keshi2017@gmail.com>
2021-12-02 15:06:39 -0800

    feat(displaylist): add styles

--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -1,23 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "../App.css";
-function DisplayList({listContainer,setListContainer}) {
-    const deleteHandler=(ind)=> setListContainer( listContainer.filter((item,i) => i!==ind ))
-    const deleteAllHandler=()=> setListContainer(listContainer.filter((item) => item!==item ))
-    const displayList=listContainer.length?listContainer.map((item,index)=>
-        <div className="listWrap"key={index} >
-
-        <li>{item}</li>
-        <button onClick={()=>deleteHandler(index)}>delete</button>
-
-        </div>    
-        
-    ):""
-    return (
-        <ol id="display-list">
-            {displayList}
-            {listContainer.length>0 &&<button onClick={()=>deleteAllHandler()}>Clear list</button>}
-        </ol>
-    )
+function DisplayList({ listContainer, setListContainer }) {
+  const deleteHandler = (ind) =>
+    setListContainer(listContainer.filter((item, i) => i !== ind));
+  const deleteAllHandler = () =>
+    setListContainer(listContainer.filter((item) => item !== item));
+  const displayList = listContainer.length
+    ? listContainer.map((item, index) => (
+        <div className="listWrap" key={index}>
+          <div className="list">
+            <p>{index + 1 + "."}</p>
+            <p id="item">{item}</p>
+          </div>
+          <button id="listbtn"onClick={() => deleteHandler(index)}>delete</button>
+        </div>
+      ))
+    : "";
+  return (
+    <div id="display-list">
+      {displayList}
+      {listContainer.length > 0 && (
+        <button onClick={() => deleteAllHandler()}>Clear list</button>
+      )}
+    </div>
+  );
 }
 
-export default DisplayList
+export default DisplayList;
