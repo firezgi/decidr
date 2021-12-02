a61f6383b06e4b95e03c520ef6d56131040d0d56
Habteab <keshi2017@gmail.com>
2021-12-01 19:22:16 -0800

    feat(displaylist): add clear all items #15

--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import "../App.css";
 function DisplayList({listContainer,setListContainer}) {
     const deleteHandler=(ind)=> setListContainer( listContainer.filter((item,i) => i!==ind ))
-    
-    const displayList=listContainer.map((item,index)=>
+    const deleteAllHandler=()=> setListContainer(listContainer.filter((item) => item!==item ))
+    const displayList=listContainer.length?listContainer.map((item,index)=>
         <div className="listWrap"key={index} >
 
         <li>{item}</li>
@@ -11,10 +11,11 @@ function DisplayList({listContainer,setListContainer}) {
 
         </div>    
         
-    )
+    ):""
     return (
         <ol id="display-list">
             {displayList}
+            {listContainer.length>0 &&<button onClick={()=>deleteAllHandler()}>Clear list</button>}
         </ol>
     )
 }
